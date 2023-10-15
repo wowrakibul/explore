5912ab84417152b1c1fdb65d7d5f420af44cac31
Wow Rakibul <74898633+wowrakibul02@users.noreply.github.com>
2023-10-15 09:50:18 -0700

    file and folder update

---
+++ script.js
@@ -0,0 +1,8 @@
+//---
+//---
+
+const githubButton = document.querySelector("#git_rep");
+
+githubButton.addEventListener("click", () => {
+  window.open("https://github.com/wowrakibul02/", "_self");
+});
